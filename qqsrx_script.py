051cc070dc95c5a7f6a88606eec7c79b11eeaf0c
bobbyesh <eshleman@pdx.edu>
2015-12-12 20:07:55 -0800

    styled according to PEP8

--- qqsrx_script.py
+++ qqsrx_script.py
@@ -1,9 +1,9 @@
-from bs4 import BeautifulSoup, NavigableString, Tag
-import urllib.request, html5lib, re, bs4
+import urllib.request
+import re
+from bs4 import BeautifulSoup
 
 
-
-def getHTML(url):
+def get_html(url):
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
         html = response.read()
@@ -11,20 +11,18 @@ def getHTML(url):
     return html
 
 
+def get_links_from_main_page():
 
-def getLinksFromMainPage():
-
-
-    def notBlogLink(href):
+    def not_blog_link(href):
         return href and not re.compile("blog").search(href)
 
-    def getPageLinkTags():
+    def get_page_link_tags():
         ''' Get the <a> tags for links to the episode transcript pages. '''
         url = 'http://phtv.ifeng.com/program/qqsrx/'
-        soup = BeautifulSoup(getHTML(url), 'html5lib')
-        return soup.find_all("a", string=re.compile("详细"), href=notBlogLink)
+        soup = BeautifulSoup(get_html(url), 'html5lib')
+        return soup.find_all("a", string=re.compile("详细"), href=not_blog_link)
 
-    taglist = getPageLinkTags()
+    taglist = get_page_link_tags()
     urls = []
     for tag in taglist:
         urls.append(tag['href'])
@@ -32,53 +30,53 @@ def getLinksFromMainPage():
     return urls
 
 
-def getLinksFromEpisodePage(firstPage):
-    soup = BeautifulSoup(getHTML(firstPage), 'html5lib')
-    otherPageTags = soup.find_all("a", string=re.compile("^2$|^3$|^4$|^5$"))
-    pageList = [ tag['href'] for tag in otherPageTags ]
-
-    return pageList
-
-def getUrlListForEpisode(firstPage):
-    otherPagesList = getLinksFromEpisodePage(firstPage)
-    return [firstPage] + otherPagesList
+def get_links_from_episode_page(first_page):
+    soup = BeautifulSoup(get_html(first_page), 'html5lib')
+    other_page_pages = soup.find_all("a", string=re.compile("^2$|^3$|^4$|^5$"))
+    page_list = [tag['href'] for tag in other_page_pages]
+    return page_list
 
 
-
-def getTextFromPage(url):
-
-    def hasMainContentParent(tag):
-        return (tag.find_parents(id="main_content") and 
-                not tag.has_attr('style') and
-                'p' in tag.name and
-                not tag.name == 'a' and
-                not tag.name == 'span' )
+def get_url_list_for_episode(first_page):
+    other_pages_list = get_links_from_episode_page(first_page)
+    return [first_page] + other_pages_list
 
 
-    soup = BeautifulSoup(getHTML(url), 'html5lib')
-    results = soup.find_all(hasMainContentParent)
+def get_text_from_page(url):
 
-    bodyText = ""
+    def has_main_content_parent(html_tag):
+        return (html_tag.find_parents(id="main_content") and
+                not html_tag.has_attr('style') and
+                'p' in html_tag.name and
+                not html_tag.name == 'a' and
+                not html_tag.name == 'span')
+
+    soup = BeautifulSoup(get_html(url), 'html5lib')
+    results = soup.find_all(has_main_content_parent)
+    body_text = ""
     for tag in results:
         if tag.string:
-            bodyText += tag.string +'\n'
+            body_text += tag.string + '\n'
+
+    return body_text
 
 
-    return bodyText
+def build_transcript_from(episode_url):
+    url_list = get_url_list_for_episode(episode_url)
+    transcript = ''
+    for link in url_list:
+        transcript += get_text_from_page(link)
+    return transcript
 
+
+def get_current_transcripts():
+    current_episodes = get_links_from_main_page()
+    transcript_list = []
+    for episode in current_episodes:
+        transcript = build_transcript_from(episode)
+        transcript_list.append(transcript)
+    return transcript_list
 
 
 if __name__ == '__main__':
-    currentEpisodes = getLinksFromMainPage()
-    transcripts = []
-    episodeCount = 1
-    for episode in currentEpisodes:
-        urllist = getUrlListForEpisode(episode)
-        fullTranscript = ''
-        for url in urllist:
-            fullTranscript += getTextFromPage(url)
-        print('======================================EPISODE' + str(episodeCount) + '=====================================================\n' +
-                fullTranscript +
-                '===========================================================================================')
-        episodeCount += 1
-
+    transcripts = get_current_transcripts()
