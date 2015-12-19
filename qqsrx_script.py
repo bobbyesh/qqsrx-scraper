4f0cfe34fcdce759f619bc808e57c13656ee6beb
bobbyesh <eshleman@pdx.edu>
2015-12-18 17:42:00 -0800

    reorganized code into appropriate classes

--- qqsrx_script.py
+++ qqsrx_script.py
@@ -3,55 +3,33 @@ import re
 from bs4 import BeautifulSoup
 
 
-class TranscriptFetcher:
-
-    home_url = 'http://phtv.ifeng.com/program/qqsrx/'
-    page_list = []
-
-    def get_homepage_html(self):
-        request = urllib.request.Request(self.home_url)
-        with urllib.request.urlopen(request) as response:
-            html = response.read()
-
-        return html
-
-    def get_episode_page_html(self, url):
-        request = urllib.request.Request(url)
-        with urllib.request.urlopen(request) as response:
-            html = response.read()
-
-        return html
-    def get_links_from_homepage(self):
-
-        def not_blog_link(href):
-            return href and not re.compile("blog").search(href)
-
-        def get_page_link_tags():
-            """ Get the <a> tags for links to the episode transcript pages. """
-            soup = BeautifulSoup(self.get_homepage_html(), 'html5lib')
-            return soup.find_all("a", string=re.compile("详细"), href=not_blog_link)
-
-        tag_list = get_page_link_tags()
-        urls = []
-        for tag in tag_list:
-            urls.append(tag['href'])
-
-        return urls
+def get_html(url):
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        html = response.read()
+    return html
 
 
-    def get_links_from_episode_page(self, first_page):
-        soup = BeautifulSoup(self.get_episode_page_html(first_page), 'html5lib')
-        other_page_pages = soup.find_all("a", string=re.compile("^2$|^3$|^4$|^5$"))
-        page_list = [tag['href'] for tag in other_page_pages]
-        return page_list
+class Page:
 
+    url = None
+    html = None
+    soup = None
+    body = ''
+    heading = ''
 
-    def get_url_list_for_episode(self, first_page):
-        other_pages_list = self.get_links_from_episode_page(first_page)
-        return [first_page] + other_pages_list
+    @classmethod
+    def from_url(cls, url):
+        return cls(url)
 
+    def __init__(self, url):
+        self.url = url
+        self.html = get_html(url)
+        self.soup = BeautifulSoup(self.html, 'html5lib')
+        self.get_body()
+        self.get_heading()
 
-    def get_text_from_page(self, url):
+    def get_body(self):
 
         def has_main_content_parent(html_tag):
             return (html_tag.find_parents(id="main_content") and
@@ -60,33 +38,110 @@ class TranscriptFetcher:
                     not html_tag.name == 'a' and
                     not html_tag.name == 'span')
 
-        soup = BeautifulSoup(self.get_episode_page_html(url), 'html5lib')
+        soup = BeautifulSoup(self.html, 'html5lib')
         results = soup.find_all(has_main_content_parent)
-        body_text = ""
         for tag in results:
             if tag.string:
-                body_text += tag.string + '\n'
+                self.body += tag.string + '\n'
 
-        return body_text
+    def get_heading(self):
+
+        def has_article_topic_id(tag):
+            return tag.has_attr('id') and tag['id'] == 'artical_topic'
+
+        soup = BeautifulSoup(self.html, 'html5lib')
+        results = soup.find_all(['h1', has_article_topic_id])
+        results = list(results)
+        self.heading = results[0].contents[0]
+#  rewrite all into page class!!!
 
 
-    def build_transcript_from(self, episode_url):
-        url_list = self.get_url_list_for_episode(episode_url)
-        transcript = ''
-        for link in url_list:
-            transcript += self.get_text_from_page(link)
-        return transcript
+class Episode:
+
+    @classmethod
+    def from_first_page_url(cls, url):
+        return cls(url)
+
+    def __init__(self, url):
+        self.pages = []
+        self.title = ''
+        self.transcript = ''
+        self.get_first_page(url)
+        self.get_remaining_pages()
+        self.build_transcript()
+        self.build_title()
+
+    def get_first_page(self, first_url):
+        assert(self.pages == [])
+        first_p = Page.from_url(first_url)
+        self.pages.append(first_p)
+
+    def get_remaining_pages(self):
+        remaining_pages_tags = self.first_page().soup.find_all("a", string=re.compile("^2$|^3$|^4$|^5$"))
+        remaining_pages = [tag['href'] for tag in remaining_pages_tags]
+        for link in remaining_pages:
+            new_page = Page.from_url(link)
+            self.pages.append(new_page)
+
+    def build_transcript(self):
+        for page in self.pages:
+            self.transcript += page.body
+
+    def build_title(self):
+        self.title = self.first_page().heading
+
+    def first_page(self):
+        return self.pages[0]
+
+    def __str__(self):
+        return self.title + '\n\n' + self.transcript
 
 
-    def get_current_transcripts(self):
-        current_episodes = self.get_links_from_main_page()
-        transcript_list = []
-        for episode in current_episodes:
-            transcript = self.build_transcript_from(episode)
-            transcript_list.append(transcript)
-        return transcript_list
+class TranscriptFetcher:
+
+    home_url = 'http://phtv.ifeng.com/program/qqsrx/'
+    homepage_html = None
+    episode_urls = []
+    episodes = []
+
+    def __init__(self):
+        pass
+
+    def fetch_transcripts(self):
+        self.get_homepage_html()
+        self.get_episode_urls()
+        self.build_episodes_from_urls()
+        return self.episodes
+
+    def get_homepage_html(self):
+        self.homepage_html = get_html(self.home_url)
+
+    def get_episode_urls(self):
+
+        def not_blog_link(href):
+            return href and not re.compile("blog").search(href)
+
+        def get_page_link_tags():
+            """ Get the <a> tags for links to the episode transcript pages from the homepage. """
+            self.homepage_soup = BeautifulSoup(self.homepage_html, 'html5lib')
+            return self.homepage_soup.find_all("a", string=re.compile("详细"), href=not_blog_link)
+
+        tag_list = get_page_link_tags()
+        for tag in tag_list:
+            self.episode_urls.append(tag['href'])
+
+    def build_episodes_from_urls(self):
+        for url in self.episode_urls:
+            new_episode = Episode.from_first_page_url(url)
+            self.episodes.append(new_episode)
 
 
 if __name__ == '__main__':
-    scraper = TranscriptFetcher()
-    transcripts = scraper.get_current_transcripts()
+    fetcher = TranscriptFetcher()
+    episodes = fetcher.fetch_transcripts()
+    ep_num = 1
+    for e in episodes:
+        print("Episode " + str(ep_num))
+        for p in e.pages:
+            print(p.url)
+        ep_num += 1
